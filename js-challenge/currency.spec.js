f7bdce41706044006852fc4cd119d691593b9015
JesterCharles <JesterCharles@gmail.com>
2022-05-20 09:50:45 -0400

    Added in the new js-challenges to the assignments repo

---
+++ js-challenge/currency.spec.js
@@ -0,0 +1,18 @@
+// write a function that takes in a number and gives back the output in USD
+// with appropriate precision
+// negative amounts should throw an error
+
+function usd(amount = 0){
+
+}
+
+test("100 dollars", ()=>{
+    const result = usd(100)
+    expect(result).toBe("$100.00")
+})
+
+test("91.3688 dollars", ()=>{
+    const result = usd(91.3688)
+    expect(result).toBe('$91.37')
+})
+
---
+++ js-challenge/no-screaming.spec.js
@@ -0,0 +1,24 @@
+// A string is considered "screaming" if 50% or more of its characters are capitalized
+// noScreams is a function that should take in an array of strings and return an array with all screams removed
+
+// FORBIDDEN KEYWORDS
+// for, while
+
+function noScreams(ray){
+
+}
+
+test("All Screaming", ()=>{
+    const result = noScreams(["AAAA","BBBB", "CCCC"])
+    expect(result.length).toBe(0)
+}) 
+
+test("None Screaming", ()=>{
+    const result = noScreams(["aaaa","bbbb", "cccc"])
+    expect(result.length).toBe(3)
+}) 
+
+test("Some Screaming", ()=>{
+    const result = noScreams(["aaaA","BBBb", "cccC"])
+    expect(result.length).toBe(2)
+}) 
